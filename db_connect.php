2f79d013b690237183dc52742a4acced7378bfee
Tim Pavlov <tema.pavlov.00@gmail.com>
2018-02-05 20:58:39 +0200

    second commit

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,31 @@
 <?php
     require "libs/rb.php";
-    R::setup('sqlite:user-store.db');
+
+    class MyORM extends SQLite3
+    {
+        public function __construct($filename)
+        {
+            $this->open($filename);
+        }
+
+        function find($table_name, $condition, $variables)
+        {
+            while ($position = strpos($condition, '?'))
+            {
+                $condition = substr($condition, 0, $position - 1) .
+                    "'" . array_shift($variables) . "'" .
+                    substr($condition, $position + 1);
+
+                array_splice($variables, $position, 1);
+            }
+
+            echo $condition;
+
+            $sqlite_result = $this->query("SELECT * FROM $table_name WHERE $condition");
+            return $sqlite_result->fetchArray();
+        }
+    }
+
+    $x = new MyORM("D:\phpProject\itstep_php_project\user-store.db");
+    $result = $x->find("user", "username = ?", array('Clinton'));
+    print_r($result);
