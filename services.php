6b2d17eb3d6d71c6a3c9a269f39f0249b45dfcf3
Tim Pavlov <tema.pavlov.00@gmail.com>
2018-03-05 17:56:14 +0200

    starting refactoring

--- services.php
+++ services.php
@@ -2,6 +2,11 @@
     $url = $_GET['url'];
     $separated_url = explode('/', $url);
 
-    foreach ($separated_url as $url_part) {
+    if (count($separated_url) > 2)
+    {
         
+    }
+    else
+    {
+
     }
